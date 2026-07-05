c40307555ac3b04c7d967c4f9d8f2802446226a2
MiracPaker <42077549+MiracPaker@users.noreply.github.com>
2026-07-06 00:11:57 +0300

    Update zara.py

--- app/zara.py
+++ app/zara.py
@@ -1,2 +1,29 @@
-def check_zara():
-    return "Henüz hazır değil"
+import requests
+
+
+def check_zara(product):
+
+    url = (
+        f"https://www.zara.com/itxrest/1/catalog/store/"
+        f"{product['store']}/product/id/"
+        f"{product['productId']}/availability"
+    )
+
+    response = requests.get(url, timeout=30)
+
+    data = response.json()
+
+    in_stock = False
+
+    for sku in data["skusAvailability"]:
+
+        if sku["availability"] != "out_of_stock":
+            in_stock = True
+            break
+
+    return {
+        "name": product["name"],
+        "url": product["url"],
+        "size": product["size"],
+        "instock": in_stock
+    }
