e226e8eab123b3e866ca6481694cdc6252692f9b
MiracPaker <42077549+MiracPaker@users.noreply.github.com>
2026-07-05 19:48:08 +0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,1 +1,24 @@
-print("Stock Tracker starting...")
+import os
+import requests
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+if not TOKEN:
+    raise Exception("TELEGRAM_BOT_TOKEN bulunamadı")
+
+if not CHAT_ID:
+    raise Exception("TELEGRAM_CHAT_ID bulunamadı")
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+r = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "✅ Stock Tracker başarıyla çalıştı."
+    },
+    timeout=30,
+)
+
+print(r.text)
